4d7ec9b766f922fea5f60e82ed7d985d6d464afe
ALMA Zakaria <zakariaalma.business@gmail.com>
2025-06-22 22:33:11 +0100

    feat(master):Create new react app - Add BrowserRouter - create new folder pages in src - create Home.jsx  - create n ew folder components in src create navbar.jsx - create Route.jsx in  the home.jsx page

---
+++ src/components/navbar.jsx
@@ -0,0 +1,41 @@
+import { Link } from "react-router-dom";
+
+
+const Navbar = () => {
+  return (
+    <div className="grid grid-cols-3 gap-4 bg-black">
+      <div className="flex justify-start pl-8">
+          <img  src={"https://screendy-cdn.fra1.cdn.digitaloceanspaces.com/platfrom-v2/_files/file_1750430583519_WhiteWolfLogo.png"} alt="logo" className="w-14 h-14" />
+      </div>
+      <div className="flex justify-center items-center "> 
+        <ul className="flex gap-4">
+          <li className="text-white text-md">
+          <Link  to="/">Home</Link>
+          </li>
+          <li className="text-white text-md">
+          <Link  to="/">Contact</Link>
+          </li>
+          <li className="text-white text-md">
+          <Link  to="/">Products</Link>
+          </li>
+        </ul>
+      </div>
+
+      <div className="flex justify-end items-center pr-12"> 
+        <ul className="flex gap-4">
+          <li className="text-white text-md">
+          <Link to="/login" >Login</Link> 
+          </li>
+          <li className="text-white text-md">
+          <Link to="/register">Register</Link>
+          </li>
+          <li className="text-white text-md">
+          <Link  to="/card">Card</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export  default Navbar; //It looks like you're trying to create a simple navigation bar component using React and Tailwind CSS. Here's a refined version of your `Navbar` component:Navbar;
---
+++ src/pages/home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/navbar.jsx";
+
+const Home = () => {
+  return (
+    <div className="relative flex flex-col full-width bg-black">
+      <Navbar/>
+    </div>
+  );
+};
+export default Home;
+
